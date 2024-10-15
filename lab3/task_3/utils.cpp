2f550dfcac40d1744401b197e68555755c5f9519
RLafleet <maksim.nazarov@ispring.institute>
2024-10-15 13:50:28 +0300

    Форматирование

--- lab3/task_3/utils.cpp
+++ lab3/task_3/utils.cpp
@@ -15,52 +15,65 @@
 #include <fstream>
 
 template <typename T>
-void SendData(int fd, const T& data) {
+void SendData(int fd, const T& data) 
+{
     auto buffer = std::bit_cast<const char*>(&data);
-    if (write(fd, buffer, sizeof(T)) == -1) {
+    if (write(fd, buffer, sizeof(T)) == -1) 
+    {
         throw std::system_error(errno, std::generic_category(), "Write to pipe failed");
     }
 }
 
 template <typename T>
-T ReceiveData(int fd) {
+T ReceiveData(int fd) 
+{
     T data;
     auto buffer = std::bit_cast<char*>(&data);
-    if (read(fd, buffer, sizeof(T)) == -1) {
+    if (read(fd, buffer, sizeof(T)) == -1) 
+    {
         throw std::system_error(errno, std::generic_category(), "Read from pipe failed");
     }
     return data;
 }
 
-void SendMessage(int fd, const std::string& message) {
-    if (write(fd, message.c_str(), message.size() + 1) == -1) {
+void SendMessage(int fd, const std::string& message) 
+{
+    if (write(fd, message.c_str(), message.size() + 1) == -1) 
+    {
         throw std::system_error(errno, std::generic_category(), "Write to pipe failed");
     }
 }
 
-std::string ReceiveMessage(int fd) {
+std::string ReceiveMessage(int fd) 
+{
     char buffer[256];
     ssize_t bytesRead = read(fd, buffer, sizeof(buffer));
-    if (bytesRead == -1) {
+    if (bytesRead == -1) 
+    {
         throw std::system_error(errno, std::generic_category(), "Read from pipe failed");
     }
     return std::string(buffer);
 }
 
-std::string HandleAdd(const std::vector<int>& numbers) {
+std::string HandleAdd(const std::vector<int>& numbers) 
+{
     int sum = std::accumulate(numbers.begin(), numbers.end(), 0);
     return "sum is " + std::to_string(sum);
 }
 
-std::string HandleLongestWord(const std::string& filename) {
+std::string HandleLongestWord(const std::string& filename) 
+{
     std::ifstream file(filename);
-    if (!file) {
+    if (!file) 
+    {
         return "Error: unable to open file";
     }
 
     std::string longestWord, word;
-    while (file >> word) {
-        if (word.length() > longestWord.length()) {
+    while (file >> word) 
+    {
+        if (word.length() > longestWord.length()) 
+        {
             longestWord = word;
         }
     }
