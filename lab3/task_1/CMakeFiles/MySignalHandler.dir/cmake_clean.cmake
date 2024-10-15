4e46f0c014bab72de7f96d6567fa7903208af777
RLafleet <maksim.nazarov@ispring.institute>
2024-10-15 11:21:39 +0300

    task_1

---
+++ lab3/task_1/CMakeFiles/MySignalHandler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySignalHandler.dir/main.cpp.o"
+  "CMakeFiles/MySignalHandler.dir/main.cpp.o.d"
+  "MySignalHandler"
+  "MySignalHandler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySignalHandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
