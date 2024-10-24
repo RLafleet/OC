e39392095182cecd050b45c356994bb5fbd7b5d1
RLafleet <maksim.nazarov@ispring.institute>
2024-10-24 08:55:44 +0300

    Добавил 2 задание 4 лабы

---
+++ lab4/task_2/task_2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <stop_token>
+#include <syncstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+
+std::string CurrentTimeToString() 
+{
+    using Clock = std::chrono::system_clock;
+    auto t = Clock::to_time_t(Clock::now());
+    struct tm buf;
+#ifdef _MSC_VER
+    localtime_s(&buf, &t);
+#else
+    localtime_r(&t, &buf);
+#endif
+    std::ostringstream os;
+    os << std::setw(2) << std::setfill('0') << buf.tm_hour << ":"
+        << std::setw(2) << std::setfill('0') << buf.tm_min << ":"
+        << std::setw(2) << std::setfill('0') << buf.tm_sec;
+    return os.str();
+}
+
+void Tic(std::stop_token st) 
+{
+    while (!st.stop_requested()) 
+    {
+        {
+            std::osyncstream sync_out(std::cout);
+            sync_out << CurrentTimeToString() << " Tic" << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+    std::osyncstream(std::cout) << "Tic thread completed" << std::endl;
+}
+
+void Tac(std::stop_token st) 
+{
+    while (!st.stop_requested()) 
+    {
+        {
+            std::osyncstream sync_out(std::cout);
+            sync_out << CurrentTimeToString() << " Tac" << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+    }
+    std::osyncstream(std::cout) << "Tac thread completed" << std::endl;
+}
+
+int main() {
+    std::jthread tic_thread(Tic);
+    std::jthread tac_thread(Tac);
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    // `std::jthread` автоматически передает stop_token в функцию потока
+
+    return 0;
+}
