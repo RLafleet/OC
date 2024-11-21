566219f981305c785eedf7d6429f8b705d0253c4
RLafleet <maksim.nazarov@ispring.institute>
2024-11-21 13:35:45 +0300

    update lab 4

--- lab4/task_2/task_2.cpp
+++ lab4/task_2/task_2.cpp
@@ -7,51 +7,57 @@
 #include <iomanip>
 #include <string>
 
-std::string CurrentTimeToString() 
+std::string CurrentTimeToString()
 {
-    using Clock = std::chrono::system_clock;
-    auto t = Clock::to_time_t(Clock::now());
-    struct tm buf;
-    localtime_s(&buf, &t);
-    std::ostringstream os;
-    os << std::setw(2) << std::setfill('0') << buf.tm_hour << ":"
-        << std::setw(2) << std::setfill('0') << buf.tm_min << ":"
-        << std::setw(2) << std::setfill('0') << buf.tm_sec;
-    return os.str();
+	using Clock = std::chrono::system_clock;
+	auto t = Clock::to_time_t(Clock::now());
+	struct tm buf;
+	localtime_s(&buf, &t);
+	std::ostringstream os;
+	os << std::setw(2) << std::setfill('0') << buf.tm_hour << ":"
+		<< std::setw(2) << std::setfill('0') << buf.tm_min << ":"
+		<< std::setw(2) << std::setfill('0') << buf.tm_sec;
+	return os.str();
 }
 
-void Tic(std::stop_token st)  
+void Tic(std::stop_token st)
 {
-    while (!st.stop_requested()) 
-    {
-        {
-            std::osyncstream sync_out(std::cout);
-            sync_out << CurrentTimeToString() << " Tic" << std::endl;
-        }
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-    }
-    std::osyncstream(std::cout) << "Tic thread completed" << std::endl;
+	while (!st.stop_requested())
+	{
+		{
+			std::osyncstream sync_out(std::cout);
+			sync_out << CurrentTimeToString() << " Tic" << std::endl;
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+	}
+	std::osyncstream(std::cout) << "Tic thread completed" << std::endl;
 }
 
-void Tac(std::stop_token st) 
+void Tac(std::stop_token st)
 {
-    while (!st.stop_requested()) 
-    {
-        {
-            std::osyncstream sync_out(std::cout);
-            sync_out << CurrentTimeToString() << " Tac" << std::endl;
-        }
-        std::this_thread::sleep_for(std::chrono::seconds(3));
-    }
-    std::osyncstream(std::cout) << "Tac thread completed" << std::endl;
+	while (!st.stop_requested())
+	{
+		{
+			std::osyncstream sync_out(std::cout);
+			sync_out << CurrentTimeToString() << " Tac" << std::endl;
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+	}
+	std::osyncstream(std::cout) << "Tac thread completed" << std::endl;
 }
 
-int main() {
-    std::jthread tic_thread(Tic);
-    std::jthread tac_thread(Tac);
+int main()
+{
+	{
+		std::stop_source ss;
+		std::jthread tic_thread(Tic, ss.get_token());
+		std::jthread tac_thread(Tac);
 
-    std::string input;
-    std::getline(std::cin, input);
-    
-    return 0;
+		std::string input;
+		std::getline(std::cin, input);
+		ss.request_stop();
+	}
+
+
+	return 0;
 }
