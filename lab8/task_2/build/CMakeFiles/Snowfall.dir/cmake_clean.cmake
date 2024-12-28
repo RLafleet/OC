803d90020b5fb9a14643e9d67b59cde5aa431313
RLafleet <maksim.nazarov@ispring.institute>
2024-12-28 12:49:45 +0300

    cmake for 2 task

---
+++ lab8/task_2/build/CMakeFiles/Snowfall.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snowfall.dir/Display.cpp.o"
+  "CMakeFiles/Snowfall.dir/Display.cpp.o.d"
+  "CMakeFiles/Snowfall.dir/EventHandler.cpp.o"
+  "CMakeFiles/Snowfall.dir/EventHandler.cpp.o.d"
+  "CMakeFiles/Snowfall.dir/Snowflake.cpp.o"
+  "CMakeFiles/Snowfall.dir/Snowflake.cpp.o.d"
+  "CMakeFiles/Snowfall.dir/main.cpp.o"
+  "CMakeFiles/Snowfall.dir/main.cpp.o.d"
+  "Snowfall"
+  "Snowfall.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snowfall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab8/task_2/build/CMakeFiles/Snowfall.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Snowfall.
+# This may be replaced when dependencies are built.
